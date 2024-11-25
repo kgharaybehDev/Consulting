9117677bdb17ae27858fed5f1af305cfa601c4ce
kgharaybehDev <kgharaybeh@outlook.com>
2024-11-25 12:05:16 +0300

    Update nginx configuration folder name
    Changed FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS from "core" to "myproject" in the nginx.sh script. This modification ensures the script points to the correct directory for the nginx configuration file.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 PROJECT_MAIN_DIR_NAME="Consulting"
 
 # Replace {FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS} with the folder name where your nginx configuration file exists
-FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS="core"
+FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS="myproject"
 
 # Reload systemd daemon
 sudo systemctl daemon-reload
